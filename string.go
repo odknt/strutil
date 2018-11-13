c64fd2545317205b9515b727884fa984f6e2987d
odknt <4185305+odknt@users.noreply.github.com>
2018-11-13 14:35:08 +0900

    add DefExpr(), DefExprStringer()

--- string.go
+++ string.go
@@ -5,17 +5,33 @@ import (
 )
 
 // Def returns def if str is empty.
-func Def(str string, def string) string {
+func Def(str, def string) string {
 	if len(str) == 0 {
 		return def
 	}
 	return str
 }
 
-// DefStringer returns def if stringer is nil or empty.
+// DefExpr returns def if expr is false else str.
+func DefExpr(expr bool, str, def string) string {
+	if expr {
+		return str
+	}
+	return def
+}
+
+// DefStringer returns def if fmt.Stringer is nil.
 func DefStringer(str fmt.Stringer, def string) string {
 	if str == nil {
 		return def
 	}
 	return Def(str.String(), def)
+}
+
+// DefExprStringer returns def if expr is false or Stringer is nil else call String().
+func DefExprStringer(expr bool, str fmt.Stringer, def string) string {
+	if expr {
+		return DefStringer(str, def)
+	}
+	return def
 }
--- string_test.go
+++ string_test.go
@@ -1,6 +1,7 @@
 package strutil
 
 import (
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,6 +20,11 @@ func TestDef(t *testing.T) {
 	assert.Equal(t, "default", Def("", "default"))
 }
 
+func TestDefExpr(t *testing.T) {
+	assert.Equal(t, "str", DefExpr(1 == 1, "str", "default"))
+	assert.Equal(t, "default", DefExpr(1 == 2, "str", "default"))
+}
+
 func TestDefStringer(t *testing.T) {
 	assert.Equal(t, "default", DefStringer(nil, "default"))
 
@@ -27,4 +33,12 @@ func TestDefStringer(t *testing.T) {
 
 	test = &testStringer{val: "test"}
 	assert.Equal(t, "test", DefStringer(test, "default"))
+}
+
+func TestDefExprStringer(t *testing.T) {
+	// net.ParseIP returns zero-length net.IP instead of nil when parse failed.
+	ip := net.ParseIP("invalid")
+	assert.Equal(t, "127.0.0.1", DefExprStringer(len(ip) > 0, ip, "127.0.0.1"))
+	ip = net.ParseIP("0.0.0.0")
+	assert.Equal(t, "0.0.0.0", DefExprStringer(len(ip) > 0, ip, "127.0.0.1"))
 }

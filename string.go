f97dd84f4fb1d5f654bc124b81efc6e704ece920
odknt <4185305+odknt@users.noreply.github.com>
2018-11-13 13:51:27 +0900

    add DefStringer() that returns default value if fmt.Stringer is nil or return empty string

--- string.go
+++ string.go
@@ -1,9 +1,21 @@
 package strutil
 
+import (
+	"fmt"
+)
+
 // Def returns def if str is empty.
 func Def(str string, def string) string {
 	if len(str) == 0 {
 		return def
 	}
 	return str
+}
+
+// DefStringer returns def if stringer is nil or empty.
+func DefStringer(str fmt.Stringer, def string) string {
+	if str == nil {
+		return def
+	}
+	return Def(str.String(), def)
 }
--- string_test.go
+++ string_test.go
@@ -6,7 +6,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testStringer struct {
+	val string
+}
+
+func (t testStringer) String() string {
+	return t.val
+}
+
 func TestDef(t *testing.T) {
 	assert.Equal(t, "str", Def("str", "default"))
 	assert.Equal(t, "default", Def("", "default"))
+}
+
+func TestDefStringer(t *testing.T) {
+	assert.Equal(t, "default", DefStringer(nil, "default"))
+
+	test := &testStringer{}
+	assert.Equal(t, "default", DefStringer(test, "default"))
+
+	test = &testStringer{val: "test"}
+	assert.Equal(t, "test", DefStringer(test, "default"))
 }

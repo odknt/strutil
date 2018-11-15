0aef99e702bf7574948726d958ecd6445ecbd76d
odknt <4185305+odknt@users.noreply.github.com>
2018-11-15 19:28:44 +0900

    add IsAlnum() and ValidSubDomain() functions

--- bytes.go
+++ bytes.go
@@ -13,3 +13,33 @@ func FromBytes(bs []byte) string {
 func ToBytes(str string) []byte {
 	return *(*[]byte)(unsafe.Pointer(&str))
 }
+
+// IsAlnum returns true if given byte slice contains only alphabets or numbers.
+func IsAlnum(bs []byte) bool {
+	for _, b := range bs {
+		if (b < 0x41 || 0x5A < b) && (b < 0x61 || 0x7A < b) && (b < 0x30 || 0x39 < b) {
+			return false
+		}
+	}
+	return true
+}
+
+// ValidSubDomain returns true if given byte slice meet the conditions below.
+// * starts with a lowercase alphabet.
+// * contains only lowercase alphabets or numbers.
+// * ends with a lowecase alphanets or numbers.
+func ValidSubDomain(bs []byte) bool {
+	if bs[0] < 0x61 || 0x7A < bs[0] {
+		return false
+	}
+	end := len(bs) - 1
+	if bs[end] == 0x2D {
+		return false
+	}
+	for _, b := range bs[1:end] {
+		if (b < 0x61 || 0x7A < b) && (b < 0x30 || 0x39 < b) && (b != 0x2D) {
+			return false
+		}
+	}
+	return true
+}
--- bytes_test.go
+++ bytes_test.go
@@ -27,3 +27,47 @@ func TestToBytes(t *testing.T) {
 	strptr := *(*reflect.SliceHeader)(unsafe.Pointer(&str))
 	assert.Equal(t, bsptr.Data, strptr.Data)
 }
+
+func TestIsAlnum(t *testing.T) {
+	strs := []struct {
+		str []byte
+		ret bool
+	}{
+		{[]byte("az"), true},
+		{[]byte("abc"), true},
+		{[]byte("a0"), true},
+		{[]byte("AZ"), true},
+		{[]byte("Zc0"), true},
+		{[]byte("aa9"), true},
+		{[]byte("0ba"), true},
+		{[]byte("9ca"), true},
+		{[]byte("9dZ"), true},
+		{[]byte("0ea"), true},
+		{[]byte("-"), false},
+		{[]byte("a-"), false},
+		{[]byte("-z"), false},
+		{[]byte("a.z"), false},
+	}
+	for _, test := range strs {
+		assert.Equal(t, test.ret, IsAlnum(test.str))
+	}
+}
+
+func TestValidSubDomain(t *testing.T) {
+	strs := []struct {
+		str []byte
+		ret bool
+	}{
+		{[]byte("azz"), true},
+		{[]byte("a-b"), true},
+		{[]byte("a0b"), true},
+		{[]byte("zb9"), true},
+		{[]byte("0ab"), false},
+		{[]byte("a.b"), false},
+		{[]byte("aZz"), false},
+		{[]byte("aZ-"), false},
+	}
+	for _, test := range strs {
+		assert.Equal(t, test.ret, ValidSubDomain(test.str))
+	}
+}
